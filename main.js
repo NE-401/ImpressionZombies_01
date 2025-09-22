148f9d536bdcc9f2fa70939d4705fce11421573b
NE-401 <phoenix00sm@gmail.com>
2025-09-22 19:48:12 +0900

    tweak main.js

--- main.js
+++ main.js
@@ -1,43 +1,28 @@
-function generateHtml(path) {
-	const p = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/' + path;
+window.addEventListener('DOMContentLoaded', function() {
+	const currentPath = window.location.pathname;
+	const pattern = /^.*\//g;
+	const htmlPath = currentPath.replace(pattern, '');
+	const JSONPath = htmlPath.replace('html','json');
+	const p = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/' + JSONPath;
 
 	fetch(p).then(response => response.json()).then(data => {
-		// load success
 		const titleString = 'Impression zombies found at ' + (data.startDate + ' ~ ' + data.endDate);
 
 		document.write('<title>' + titleString + '</title>');
 		document.write('<h1>' + titleString + '</h1>');
 
 		const userKeys = Object.keys(data.userIds);
-
 		for(let i = 0; i < userKeys.length; i++) {
 			userName = data.userIds[i];
 			document.write(
 				String(i + 1) + ': <a href="https://x.com/' + userName + '">' + userName + '</a><br>'
 			);
 		}
-
 		document.write('<br><br><a href="index.html">Back to index.html</a>');
 	}).catch(error => {
-		console.error('Error:', error);
-		const titleStringFailed = 'JSON load failed';
-		// title
-		const title = document.createElement('title');
-		title.textContent = titleStringFailed;
-		document.head.appendChild(title);
-
-		// h1
-		const h1 = document.createElement('h1');
-		h1.textContent = titleStringFailed;
-		document.body.appendChild(h1);
+		// console.error('Error:', error);
+		document.write('<title>JSON load failed</title>');
+		document.write('<h1>JSON load failed</h1>');
 	});
-}
 
-window.addEventListener('DOMContentLoaded', function() {
-	const currentPath = window.location.pathname;
-	const pattern = /^.*\//g;
-	const htmlPath = currentPath.replace(pattern, '');
-	const JSONPath = htmlPath.replace('html','json');
-
-	generateHtml(JSONPath);
 });
