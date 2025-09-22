60feb2ca026c58f9bbed8366765511aa2813572a
NE-401 <phoenix00sm@gmail.com>
2025-09-22 20:08:10 +0900

    some tweaks

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
 	const currentPath = window.location.pathname;
 	const pattern = /^.*\//g;
-	const htmlPath = currentPath.replace(pattern, '');
-	const jsonPath = htmlPath.replace('html','json');
-	const jsonUrl = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/' + jsonPath;
+	const url = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/';
+	const jsonName = currentPath.replace(pattern, '').replace('html','json');
+	const jsonUrl = url + jsonName;
 
 	fetch(jsonUrl).then(response => response.json()).then(data => {
 		const titleString = 'Impression zombies found at ' + (data.startDate + ' ~ ' + data.endDate);
