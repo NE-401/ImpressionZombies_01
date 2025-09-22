91da19dc4241a2cc279c6cdd117217c4d8365601
NE-401 <phoenix00sm@gmail.com>
2025-09-22 19:50:13 +0900

    tweak main.js again :P

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ window.addEventListener('DOMContentLoaded', function() {
 	const currentPath = window.location.pathname;
 	const pattern = /^.*\//g;
 	const htmlPath = currentPath.replace(pattern, '');
-	const JSONPath = htmlPath.replace('html','json');
-	const p = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/' + JSONPath;
+	const jsonPath = htmlPath.replace('html','json');
+	const p = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/' + jsonPath;
 
 	fetch(p).then(response => response.json()).then(data => {
 		const titleString = 'Impression zombies found at ' + (data.startDate + ' ~ ' + data.endDate);
-
 		document.write('<title>' + titleString + '</title>');
 		document.write('<h1>' + titleString + '</h1>');
 
@@ -19,10 +18,8 @@ window.addEventListener('DOMContentLoaded', function() {
 			);
 		}
 		document.write('<br><br><a href="index.html">Back to index.html</a>');
-	}).catch(error => {
-		// console.error('Error:', error);
+	}).catch(error => {		// failed to load JSON
 		document.write('<title>JSON load failed</title>');
 		document.write('<h1>JSON load failed</h1>');
 	});
-
 });
