460ef3f3322fcf38bb05a79aa03883cd359c2b0f
NE-401 <phoenix00sm@gmail.com>
2025-11-10 01:12:54 +0900

    tweak script/search.js

--- script/search.js
+++ script/search.js
@@ -9,30 +9,36 @@ function onClickSearchBtn() {
 		return;
 	} else {
 		const result = document.getElementById('result');
-		// result.innerText = txt;
-
 		let i = 1;
+		let isEnd = false;
 		do {
 			const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
 			const jsonUrl = url + jsonNum + '.json';
-			fetch(jsonUrl).then(response => response.json()).then(data => {
-				const userKeys = Object.keys(data.userIds);
-				for(let i = 0; i < userKeys.length; i++) {
-					if(data.userIds[i].toLowerCase() == txt.toLowerCase()) {
-						result.innerText = txt + ' is found in the list ' + jsonNum + '.json';
-						return;
+			const xhr = new XMLHttpRequest();
+			xhr.open("HEAD", jsonUrl, false);
+			xhr.send();
+			if(xhr.status == 404) {		// not found JSON
+				result.innerText = txt + ' is not present in the list. Please contact the list owner.';
+				break;
+			} else {
+				fetch(jsonUrl).then(response => response.json()).then(data => {
+					const userKeys = Object.keys(data.userIds);
+					for(let j = 0; j < userKeys.length; j++) {
+						if(data.userIds[j].toLowerCase() == txt.toLowerCase()) {
+							result.innerText = txt + ' is found in the list ' + jsonNum + '.json';
+							isEnd = true;
+							break;
+						}
 					}
-				}
-			}).catch(error => {		// failed to load JSON, or not found JSON
-				// return 1;
-			});
-
+				}).catch(error => {		// failed to load JSON
+					result.innerText = txt + ' is not present in the list. Please contact the list owner.';
+					isEnd = true;
+				});
+			}
+			if(isEnd) break;
 			i++;
 		} while(i < 100);
-
-		result.innerText = txt + ' is not present in the list. Please contact the list owner.';
 	}
-
 }
 
 window.addEventListener('DOMContentLoaded', function() {
