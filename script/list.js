bf31847d9a4af624ae5252dbd66b831d7ab9fd97
NE-401 <phoenix00sm@gmail.com>
2025-11-19 13:56:43 +0900

    feat: add checkbox for remind blocked account (a bit unstable lol)

--- script/list.js
+++ script/list.js
@@ -1,3 +1,71 @@
+const b64char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+function intToBinaryStr(n) {
+	let s = '';
+	let m = n;
+
+	for(let b = 32; b > 1; b /= 2) {
+		if((m / b) >= 1) {
+			s += '1';
+		} else {
+			s += '0';
+		}
+
+		m -= b;
+	}
+	if(m % 2) s += '1';
+	else s += '0';
+
+	return s;
+}
+
+function binaryStrToInt(s) {
+	let n = 0;
+	n += (s.charAt(0) == '1' ? 32 : 0);
+	n += (s.charAt(1) == '1' ? 16 : 0);
+	n += (s.charAt(2) == '1' ? 8 : 0);
+	n += (s.charAt(3) == '1' ? 4 : 0);
+	n += (s.charAt(4) == '1' ? 2 : 0);
+	n += (s.charAt(5) == '1' ? 1 : 0);
+	return n;
+}
+
+function initData(pageNum, max) {
+	let s = 'A'.repeat(Math.ceil(max / 6));
+	localStorage.setItem(pageNum, s);
+	return '0'.repeat(max);
+}
+
+// compressed string to binary string
+function readData(pageNum) {
+	const result = localStorage.getItem(pageNum);
+	let expand = '';
+
+	if(result) {
+		for(let i = 0; i < result.length; i++) {
+			expand += intToBinaryStr(b64char.indexOf(result.charAt(i)));
+		}
+	} else {
+		return null;
+	}
+
+	return expand;
+}
+
+// binary string to compressed string
+function writeData(pageNum, s) {
+	let ts = s;
+	let ds = '';
+
+	while(ts.length > 0) {
+		const n = ts.slice(0,6);
+		ts = ts.slice(6);
+		ds += b64char.charAt(binaryStrToInt(n));
+	}
+
+	localStorage.setItem(pageNum, ds);
+}
+
 window.addEventListener('DOMContentLoaded', function() {
 	const currentPath = window.location.pathname;
 	const pattern = /^.*\//g;
@@ -11,14 +79,38 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		const userKeys = Object.keys(data.userIds);
 		document.write('<h1>' + titleString + ' (' + userKeys.length + ' accounts)' + '</h1><ol>');
-		document.write('<table border="1"><tr><th>#</th><th>Accound ID</th></tr>');
+		document.write('<table border="1"><tr><th>#</th><th>Accound ID</th><th>Blocked?</th></tr>');
+
+		const pageNumber = jsonName.replace('.json','');
+		let chkBoxState = readData(pageNumber);
+		if(!chkBoxState) {
+			chkBoxState = initData(pageNumber, userKeys.length);
+		}
+		let stateArray = chkBoxState.split('');
+		let chkboxArr = [];
 
 		for(let i = 0; i < userKeys.length; i++) {
 			const userName = data.userIds[i];
+			const checked = stateArray[i];
+
 			document.write(
-				'<tr><td>' + String(i+1) + '</td><td><a href="https://x.com/' + userName + '">@' + userName + '</a></td>'
+				'<tr><td>' + String(i + 1) + '</td><td><a href="https://x.com/' + userName + '">@' + userName + '</a></td>'
+				+ '<td><input type="checkbox" id="c_' + String(i)
+				+ (checked == '1' ? '" checked' : '"')
+				+ '></td></tr>'
 			);
+
+			const c = document.getElementById('c_' + String(i));
+			chkboxArr.push(c);
 		}
+
+		chkboxArr.forEach((c, i) => {
+			c.addEventListener('click', function() {
+				stateArray[i] = (c.checked) ? '1' : '0';
+				writeData(pageNumber, stateArray.join(''));
+			});
+		});
+
 		document.write('</table><br><br><a href="../index.html">Back to index.html</a>');
 	}).catch(error => {		// failed to load JSON
 		document.write('<title>JSON load failed</title>');
