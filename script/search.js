3137b5dc1fb52b75a4373712228e6cd2e3ebccfd
NE-401 <phoenix00sm@gmail.com>
2025-11-20 12:50:34 +0900

    change: changed it to load JSON only once

--- script/search.js
+++ script/search.js
@@ -1,3 +1,20 @@
+let users = [];
+
+function searchId(t) {
+	let found = false;
+	users.forEach((u) => {
+		const uArr = u[1];
+		if(!found) {
+			uArr.forEach((ud) => {
+				if(t.toLowerCase() === ud.toLowerCase()) {
+					found = u[0];
+				}
+			});
+		}
+	});
+	return found;
+}
+
 async function onClickSearchBtn() {
 	const url = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/';
 	const decimalChar = "0123456789";
@@ -8,38 +25,40 @@ async function onClickSearchBtn() {
 		return;
 	} else {
 		const result = document.getElementById('result');
-		let isEnd = false;
-		const users = [];
-		for(let i = 1; i < 100 && !isEnd; i++) {
-			const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
-			const jsonUrl = url + jsonNum + '.json';
-			const xhr = new XMLHttpRequest();
-			xhr.open("HEAD", jsonUrl, false);
-			xhr.send();
-			if(xhr.status == 404) {		// not found JSON
-				users.forEach((u) => {
-					const uArr = u[1];
-					uArr.forEach((ud) => {
-						if(!isEnd && txt.toLowerCase() === ud.toLowerCase()) {
-							result.innerText = txt + ' is found at ' + u[0] + '.html.';
-							isEnd = true;
-						}
-					});
-				});
-				if(!isEnd) result.innerText = txt + ' was not found on list.';
-				break;
+		if(users.length) {
+			const res = searchId(txt);
+			if(res) {
+				result.innerText = txt + ' was found on ' + res + '.html.';
 			} else {
-				fetch(jsonUrl).then(response => response.json()).then(data => {
-					let a = [];
-					a.push(jsonNum);
-					a.push(data.userIds);
-					users.push(a);
-					console.log('loaded ' + jsonNum + '.json');
-				}).catch(error => {		// failed to load JSON
-					isEnd = true;
-				});
+				result.innerText = txt + ' was not found on list.';
 			}
-			await sleep(4);
+		} else {
+			for(let i = 1; i < 100; i++) {
+				const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
+				const jsonUrl = url + jsonNum + '.json';
+				const xhr = new XMLHttpRequest();
+				xhr.open("HEAD", jsonUrl, false);
+				xhr.send();
+				if(xhr.status == 404) {		// not found JSON
+					const res = searchId(txt);
+					if(res) {
+						result.innerText = txt + ' was found on ' + res + '.html.';
+					} else {
+						result.innerText = txt + ' was not found on list.';
+					}
+					break;
+				} else {
+					fetch(jsonUrl).then(response => response.json()).then(data => {
+						let a = [];
+						a.push(jsonNum);
+						a.push(data.userIds);
+						users.push(a);
+					}).catch(error => {		// failed to load JSON
+						console.log(error);
+					});
+				}
+				await sleep(4);
+			}
 		}
 	}
 }
