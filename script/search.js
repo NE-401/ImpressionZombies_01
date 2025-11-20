694bf49d45b39adfde9526152c6a851b87d34803
NE-401 <phoenix00sm@gmail.com>
2025-11-20 20:57:07 +0900

    improve: change xhr -> fetch and more

--- script/search.js
+++ script/search.js
@@ -1,69 +1,61 @@
 let users = [];
 
 function searchId(t) {
+	const result = document.getElementById('result');
+	const txt = inputId.value;
 	let found = false;
 	users.forEach((u) => {
 		const uArr = u[1];
 		if(!found) {
 			uArr.forEach((ud) => {
 				if(t.toLowerCase() === ud.toLowerCase()) {
-					found = u[0];
+					result.innerText = t + ' was found on ' + u[0] + '.html.';
+					found = true;
 				}
 			});
 		}
 	});
-	return found;
+	if(!found) result.innerText = t + ' was not found on list.';
 }
 
-async function onClickSearchBtn() {
+async function loadJSON() {
 	const url = 'https://raw.githubusercontent.com/NE-401/ImpressionZombies_01/refs/heads/main/json/';
 	const decimalChar = "0123456789";
-	const txt = inputId.value;
 	const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+	let ed = '';
 
+	for(let i = 1; ed != 'now' && i < 100; i++) {
+		const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
+		const jsonUrl = url + jsonNum + '.json';
+		const res = await fetch(jsonUrl);
+		if(!res.ok) {		// some http errors
+			break;
+		} else {
+			fetch(jsonUrl).then(response => response.json()).then(data => {
+				let a = [];
+				a.push(jsonNum);
+				a.push(data.userIds);
+				users.push(a);
+				ed = data.endDate;
+			}).catch(error => {		// failed to load JSON
+				console.log(error);
+			});
+		}
+		await sleep(4);
+	}
+}
+
+function onClickSearchBtn() {
+	const txt = inputId.value;
 	if(txt.trim() === '') {
 		return;
 	} else {
-		const result = document.getElementById('result');
-		if(users.length) {
-			const res = searchId(txt);
-			if(res) {
-				result.innerText = txt + ' was found on ' + res + '.html.';
-			} else {
-				result.innerText = txt + ' was not found on list.';
-			}
-		} else {
-			for(let i = 1; i < 100; i++) {
-				const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
-				const jsonUrl = url + jsonNum + '.json';
-				const xhr = new XMLHttpRequest();
-				xhr.open("HEAD", jsonUrl, false);
-				xhr.send();
-				if(xhr.status == 404) {		// not found JSON
-					const res = searchId(txt);
-					if(res) {
-						result.innerText = txt + ' was found on ' + res + '.html.';
-					} else {
-						result.innerText = txt + ' was not found on list.';
-					}
-					break;
-				} else {
-					fetch(jsonUrl).then(response => response.json()).then(data => {
-						let a = [];
-						a.push(jsonNum);
-						a.push(data.userIds);
-						users.push(a);
-					}).catch(error => {		// failed to load JSON
-						console.log(error);
-					});
-				}
-				await sleep(4);
-			}
-		}
+		searchId(txt);
 	}
 }
 
 window.addEventListener('DOMContentLoaded', function() {
 	const submitBtn = document.getElementById('submitBtn');
 	submitBtn.addEventListener('click', onClickSearchBtn);
+	loadJSON();
 });
