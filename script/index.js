e4ad80a09bb10e3b6a7707a6cb3c159fdcb39f53
NE-401 <phoenix00sm@gmail.com>
2025-12-09 14:37:15 +0900

    feat: loading time display

--- script/index.js
+++ script/index.js
@@ -5,6 +5,7 @@ async function generateList() {
 	let ed = '', docList = '';
 	let doc = '<title>Impression zombies list</title><h2>Impression zombies list</h2>';
 	let total = 0;
+	const st = new Date();
 
 	for(let i = 1; ed != 'now' && i < 100; i++) {
 		const jsonNum = decimalChar.charAt(i / 10) + decimalChar.charAt(i % 10);
@@ -23,8 +24,10 @@ async function generateList() {
 			await sleep(1);
 		}
 	}
+	const et = new Date();
 	doc += '<h3>Total: ' + total + 'x Zombies</h3><ul>' + docList + '</ul>';
-	doc += '<br><br><a href="search.html">Search user ID</a><br></body>';
+	doc += '<p>Loading time: ' + (et.getTime() - st.getTime()) + ' ms</p>';
+	doc += '<br><a href="search.html">Search user ID</a><br></body>';
 	document.write(doc);
 }
 
