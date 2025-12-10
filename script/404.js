662041dbe1e0a6b3203308f5eaaf1d7d4f31c1ce
NE-401 <phoenix00sm@gmail.com>
2025-12-10 09:57:50 +0900

    test 404.js

--- script/404.js
+++ script/404.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const m = location.pathname.match(/^\/lists\/(.+)\.html$/i);
 	if(m) {
 		const p = m[1];
-		location.replace('/ImpressionZombies_01/list.html?p=' + p);
+		location.replace('/list.html?p=' + p);
 		throw new Error('Redirecting');
 	}
 
