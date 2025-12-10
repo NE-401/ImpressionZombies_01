80f510ee7c286e2d1c7fcfe3cdb00f7c1df3e24e
NE-401 <phoenix00sm@gmail.com>
2025-12-10 09:54:31 +0900

    test 404.js

--- script/404.js
+++ script/404.js
@@ -5,17 +5,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		const p = m[1];
 		location.replace('/ImpressionZombies_01/list.html?p=' + p);
 		throw new Error('Redirecting');
-	} else {
-		let sec = 10;
-		const timerElm = document.getElementById('timer');
-		const countdown = setInterval(() => {
-			sec--;
-			timerElm.textContent = sec;
-
-			if(sec <= 0) {
-				clearInterval(countdown);
-				location.href = '/ImpressionZombies_01';
-			}
-		}, 1000);
 	}
+
+	let sec = 10;
+	const timerElm = document.getElementById('timer');
+	const countdown = setInterval(() => {
+		sec--;
+		timerElm.textContent = sec;
+		if(sec <= 0) {
+			clearInterval(countdown);
+			location.href = '/ImpressionZombies_01';
+		}
+	}, 1000);
 });
