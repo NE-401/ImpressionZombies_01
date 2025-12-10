1b29cbb74a8a16e16efc91f1cb4402ad69e5b952
NE-401 <phoenix00sm@gmail.com>
2025-12-10 11:09:47 +0900

    fix 404 page

--- script/404.js
+++ script/404.js
@@ -1,20 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const m = location.pathname.match(/lists\/(.+)\.html$/i);
 	if(m) {
+		document.write('Redirecting to new page, please wait...');
 		const p = m[1];
 		location.replace('/ImpressionZombies_01/list.html?p=' + p);
 		throw new Error('Redirecting');
-	}
+	} else {
+		document.write('<head><title>Page not found</title></head>');
+		document.write('<body><h1>404 - Page not found.</h1>');
+		document.write('<p>The page you are looking for does not exist or the URL is incorrect.</p>');
+		document.write('You will be redirected back to the top page in <span id="timer">10</span> seconds.');
+		document.write('<br><br><a href="/ImpressionZombies_01">Return to top page now</a>');
 
-	let sec = 10;
-	const timerElm = document.getElementById('timer');
-	// console.log(location.pathname);
-	const countdown = setInterval(() => {
-		sec--;
-		timerElm.textContent = sec;
-		if(sec <= 0) {
-			clearInterval(countdown);
-			location.href = '/ImpressionZombies_01';
-		}
-	}, 1000);
+		let sec = 10;
+		const timerElm = document.getElementById('timer');
+		// console.log(location.pathname);
+		const countdown = setInterval(() => {
+			sec--;
+			timerElm.textContent = sec;
+			if(sec <= 0) {
+				clearInterval(countdown);
+				location.href = '/ImpressionZombies_01';
+			}
+		}, 1000);
+	}
 });
