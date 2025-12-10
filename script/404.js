d347651d2ab173991fd6e01f0b61db5dee82743a
NE-401 <phoenix00sm@gmail.com>
2025-12-10 10:53:26 +0900

    redirect old page

--- script/404.js
+++ script/404.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const m = location.pathname.match(/^\/lists\/(.+)\.html$/i);
+	console.log(m);
 	if(m) {
 		const p = m[1];
 		location.replace('/list.html?p=' + p);
